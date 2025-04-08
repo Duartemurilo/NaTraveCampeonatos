d7bfccbd5d0d9f3855d5638e4ce3e2f3c7f01504
Murilo duarte <murilo.duarte@talk2buy.com.br>
2025-04-08 20:30:58 -0300

    teste

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,45 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-const isStaticExport = 'false';
+/** @type {import('next').NextConfig} */
+const isStaticExport = "false";
 
 const nextConfig = {
   trailingSlash: true,
   env: {
     BUILD_STATIC_EXPORT: isStaticExport,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_ASSETS_DIR: process.env.NEXT_PUBLIC_ASSETS_DIR,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL:
+      process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL:
+      process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    SIGNING_SECRET: process.env.SIGNING_SECRET,
+    NEXT_PUBLIC_BRASILAPI_URL: process.env.NEXT_PUBLIC_BRASILAPI_URL,
   },
   modularizeImports: {
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
     },
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
     },
-    '@mui/lab': {
-      transform: '@mui/lab/{{member}}',
+    "@mui/lab": {
+      transform: "@mui/lab/{{member}}",
     },
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
 
     return config;
   },
-  ...(isStaticExport === 'true' && {
-    output: 'export',
+  ...(isStaticExport === "true" && {
+    output: "export",
   }),
 };
 
